cf85f94dd3157490eb383815e3e60b07b9112cbf
Anamika <Anamika_U@Dell.com>
2025-03-05 14:38:12 +0530

    crash issue

--- obj/Debug/net8.0/DeliveryAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DeliveryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliveryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ae87408d2f0c0477ad81e7252fa1327d5f0b52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3aced67a54374dd38f14e751e11d19452b62a35")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliveryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliveryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
