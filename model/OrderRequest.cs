c566c4b9dcd451ea67ca6676ea303225247d2f91
Anamika <Anamika_U@Dell.com>
2025-03-05 16:06:40 +0530

    clean

--- model/OrderRequest.cs
+++ model/OrderRequest.cs
@@ -4,6 +4,6 @@ using System.Text.Json.Serialization;
 public class OrderRequest
 {
     [JsonPropertyName("order")]
-    public Dictionary<string,int> Order{get; set;}
+    public Dictionary<string,int>? Order {get; set;}
 
 }
--- obj/Debug/net8.0/DeliveryAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DeliveryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliveryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce6d28d30c6e84ca4c932bd6914a9c198785d88f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e002546cafe820d554739510ac37d9de1469693")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliveryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliveryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
